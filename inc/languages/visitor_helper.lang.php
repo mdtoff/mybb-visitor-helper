a7a4a266ad9203b5c7f85aef40ca2aae42080874
Medet Erdal <45311518+mdtoff@users.noreply.github.com>
2018-11-24 15:42:59 +0300

    Create visitor_helper.lang.php

---
+++ inc/languages/visitor_helper.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Visitor Helper
+* Visitor helper on the thread display page.
+*
+* @package      MyBB
+* @author       mdtoff <mdtoff@gmail.com>
+* @version      1.0.0
+* @copyright    (c) 2018 mdtoff
+*/
+
+$l['visitor_helper_title'] = 'Bir hesap oluşturun veya yorum yapmak için giriş yapın';
+$l['visitor_helper_sub_title'] = 'Yorum yapmak için üye olmanız gerekiyor';
+
+$l['visitor_helper_register'] = 'Hesap oluştur';
+$l['visitor_helper_register_text'] = 'Topluluğumuzda yeni bir hesap için kaydolun. Çok kolay!';
+$l['visitor_helper_register_button'] = 'Yeni hesap oluştur';
+
+$l['visitor_helper_login'] = 'Giriş yap';
+$l['visitor_helper_login_text'] = 'Zaten hesabınız var mı? Giriş yapın.';
+$l['visitor_helper_login_button'] = 'Hemen giriş yap';
+
+$l['visitor_helper_or'] = 'ya da';
