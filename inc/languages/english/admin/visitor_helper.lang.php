09fa331e3b6f5a9a6d12c0fc3c36ddf550e4fe19
Medet Erdal <45311518+mdtoff@users.noreply.github.com>
2018-11-24 15:39:37 +0300

    Create visitor_helper.lang.php

---
+++ inc/languages/english/admin/visitor_helper.lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Visitor Helper
+* Visitor helper on the thread display page.
+*
+* @package      MyBB
+* @author       mdtoff <mdtoff@gmail.com>
+* @version      1.0.0
+* @copyright    (c) 2018 mdtoff
+*/
+
+$l['visitor_helper_name'] = 'Visitor Helper';
+$l['visitor_helper_desc'] = 'Visitor helper on the thread display page.';
