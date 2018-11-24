4fe856e3a380af4b9009da4cef2d6180a939cf10
Medet Erdal <45311518+mdtoff@users.noreply.github.com>
2018-11-24 15:40:30 +0300

    Create visitor_helper.lang.php

---
+++ inc/languages/english/visitor_helper.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Visitor Helper
+* Visitor helper on the thread display page.
+*
+* @package      MyBB
+* @author       mdtoff <mdtoff@gmail.com>
+* @version      1.0.0
+* @copyright    (c) 2018 mdtoff
+*/
+
+$l['visitor_helper_title'] = 'Create an account or sign in to comment';
+$l['visitor_helper_sub_title'] = 'You need to be a member in order to leave a comment';
+
+$l['visitor_helper_register'] = 'Create an account';
+$l['visitor_helper_register_text'] = 'Sign up for a new account in our community. It\'s easy!';
+$l['visitor_helper_register_button'] = 'Register a new account';
+
+$l['visitor_helper_login'] = 'Sign in';
+$l['visitor_helper_login_text'] = 'Already have an account? Sign in here.';
+$l['visitor_helper_login_button'] = 'Sign in now';
+
+$l['visitor_helper_or'] = 'or';
